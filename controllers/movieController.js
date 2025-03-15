2521bcfd422e1e59f2eb77ebbe222e9503b1c685
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-03-15 09:54:25 +0100

    add: index query

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,5 +1,14 @@
 import connection from '../data/db.js';
 
-function index(req, res) {}
+function index(req, res) {
+  const sql = 'SELECT * FROM movies';
+
+  const queryResult = (err, results) => {
+    if (err) res.status(500), json({ error: 'Server error -> Index function' });
+    else return res.json(results);
+  };
+
+  connection.query(sql, (err, results) => queryResult(err, results));
+}
 
 export { index };
