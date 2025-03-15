6a786eb69fa50372d8d82fd02c725406209bf65b
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-03-15 10:32:43 +0100

    add: show query

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -11,4 +11,41 @@ function index(req, res) {
   connection.query(sql, (err, results) => queryResult(err, results));
 }
 
-export { index };
+function show(req, res) {
+  const id = req.params.id;
+
+  const sql = 'SELECT * FROM movies WHERE id = ?';
+  const reviewsSql = 'SELECT * FROM reviews WHERE movie_id = ?';
+
+  const serverError = () => {
+    res.status(500);
+    return { error: 'Server error -> Show function' };
+  };
+
+  const getMovie = (err, results) => {
+    if (err) {
+      const error = serverError();
+      json(error);
+    } else if (results.length === 0) res.status(404), json({ 'error code': 404, error: 'Movie Not Found' });
+    else return results[0];
+  };
+
+  const getReviews = (err, results) => {
+    if (err) {
+      const error = serverError();
+      json(error);
+    } else return results;
+  };
+
+  connection.query(sql, [id], (err, results) => {
+    const movie = getMovie(err, results);
+
+    connection.query(reviewsSql, [id], (err, reviewsResult) => {
+      const reviews = getReviews(err, reviewsResult);
+      movie.reviews = reviews;
+      json(movie);
+    });
+  });
+}
+
+export { index, show };
