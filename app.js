045543812ee656fed6a36c5b4c979cb57b9b42ae
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-03-15 11:01:27 +0100

    add: in app -> middlewares

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const app = express();
 const port = process.env.SERVER_PORT;
 
 import router from './routers/movieRouter.js';
+import imgPathMw from './middlewares/imagePath.js';
+
+//middlewares
+app.use(express.static('public'));
+app.use(express.json());
+app.use(imgPathMw);
 
 app.use('/books', router);
 
