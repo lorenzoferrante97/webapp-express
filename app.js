4cd6b0cd7e0569f538bb7dd06b95bb7d8f1e8540
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-03-18 11:42:12 +0100

    add: cors for frontend

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 const port = process.env.SERVER_PORT;
 
 import router from './routers/movieRouter.js';
 import imgPathMw from './middlewares/imagePath.js';
+
+// cors
+app.use(cors({ origin: process.env.FRONTEND_APP }));
 
 //middlewares
 app.use(express.static('public'));
